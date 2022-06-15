38431631bf1a1688cc4acf884169f53f20d5664d
Ben Kiboma <omayioben@gmail.com>
2022-06-15 20:17:50 +0300

    Style UI

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 import '@fortawesome/fontawesome-free/css/all.css';
 import '@fortawesome/fontawesome-free/js/all.js';
 import './main.css';
+
+//Reference HTML
+const myInput = document.querySelector('input');
+const recentSores = document.querySelector('#recent-scores');
+
+const allScores = [
+  {"name":"Man-City","Score":"80"},
+  {"name":"Manchester United","Score":"75"},
+  {"name":"Liverpool","Score":"82"},
+  {"name":"Arsenal","Score":"56"},
+  {"name":"Barcelona","Score":"40"},
+  {"name":"Everton","Score":"76"}
+];
+
