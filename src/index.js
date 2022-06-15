aacc6023109d770360d4e61e9fb85ca6f1fe78aa
Ben Kiboma <omayioben@gmail.com>
2022-06-16 00:15:04 +0300

    Remove linter errors

--- src/index.js
+++ src/index.js
@@ -1,25 +1,27 @@
+/* eslint-disable no-plusplus */
+/* eslint-disable import/extensions */
 import '@fortawesome/fontawesome-free/css/all.css';
 import '@fortawesome/fontawesome-free/js/all.js';
 import './main.css';
 
 const allScores = [
-  {"name":"Man-City","Score":"80"},
-  {"name":"Manchester United","Score":"75"},
-  {"name":"Liverpool","Score":"82"},
-  {"name":"Arsenal","Score":"56"},
-  {"name":"Barcelona","Score":"40"},
-  {"name":"Everton","Score":"76"}
+  { name: 'Man-City', Score: '80' },
+  { name: 'Manchester United', Score: '75' },
+  { name: 'Liverpool', Score: '82' },
+  { name: 'Arsenal', Score: '56' },
+  { name: 'Barcelona', Score: '40' },
+  { name: 'Everton', Score: '76' },
 ];
 
-var recentSores = document.querySelector('#recent-scores');
+const recentSores = document.querySelector('#recent-scores');
 
-  for (let i=0; i<allScores.length; i++) {
-    var displayData = `<tr>
+for (let i = 0; i < allScores.length; i++) {
+  const displayData = `<tr>
     <td>${allScores[i].name}</td>
     <td>${allScores[i].Score}</td>
     </tr>
-    `
-    recentSores.innerHTML +=displayData;
-  }
+    `;
+  recentSores.innerHTML += displayData;
+}
 
-document.getElementById("heading").innerHTML = "Leaderboard";
+document.getElementById('heading').innerHTML = 'Leaderboard';
